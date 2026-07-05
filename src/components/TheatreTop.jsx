774d39730ff3ec9b3fc5f02459aed6a584afa099
s-vorobev <svorobyovs357@gmail.com>
2026-07-05 17:11:57 -0500

    Add caution tape to bottom part of theatre

---
+++ src/components/TheatreTop.jsx
@@ -0,0 +1,17 @@
+import './TheatreTop.css'
+
+function TheatreTop() {
+  return (
+    <section className="page theatre-top">
+      <svg
+        className="page-bg"
+        viewBox="0 0 6010 2580"
+        preserveAspectRatio="xMidYMid slice"
+      >
+        <image href="/theatre_top.png" x="0" y="0" width="6010" height="2580" />
+      </svg>
+    </section>
+  )
+}
+
+export default TheatreTop
