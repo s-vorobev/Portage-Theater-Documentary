3351b23ee6d44c2620c53bbb8460cb03b0ddc40b
s-vorobev <svorobyovs357@gmail.com>
2026-07-06 00:16:35 -0500

    Use Prettier to format code

--- src/components/TheatreBottom.jsx
+++ src/components/TheatreBottom.jsx
@@ -1,6 +1,6 @@
-import TheatreScene from './scene/TheatreScene'
-import SceneAsset from './scene/SceneAsset'
-import { ASSETS } from '../lib/sceneAssets'
+import TheatreScene from "./scene/TheatreScene";
+import SceneAsset from "./scene/SceneAsset";
+import { ASSETS } from "../lib/sceneAssets";
 
 function TheatreBottom() {
   return (
@@ -10,10 +10,22 @@ function TheatreBottom() {
       bgWidth={6010}
       bgHeight={2580}
     >
-      <SceneAsset asset={ASSETS.cautionTape} centerX={3005} centerY={1985} scale={4.8} rotateDeg={6} />
-      <SceneAsset asset={ASSETS.cautionTape} centerX={3005} centerY={1985} scale={4.8} rotateDeg={-6} />
+      <SceneAsset
+        asset={ASSETS.cautionTape}
+        centerX={3005}
+        centerY={1985}
+        scale={4.8}
+        rotateDeg={6}
+      />
+      <SceneAsset
+        asset={ASSETS.cautionTape}
+        centerX={3005}
+        centerY={1985}
+        scale={4.8}
+        rotateDeg={-6}
+      />
     </TheatreScene>
-  )
+  );
 }
 
-export default TheatreBottom
+export default TheatreBottom;
--- src/components/scene/TheatreScene.jsx
+++ src/components/scene/TheatreScene.jsx
@@ -1,15 +1,22 @@
-import './TheatreScene.css'
+import "./TheatreScene.css";
 
-function TheatreScene({ bgSrc, bgWidth, bgHeight, className, children, overlay }) {
+function TheatreScene({
+  bgSrc,
+  bgWidth,
+  bgHeight,
+  className,
+  children,
+  overlay,
+}) {
   return (
-    <section className={`page ${className ?? ''}`}>
+    <section className={`page ${className ?? ""}`}>
       <svg className="page-bg" viewBox={`0 0 ${bgWidth} ${bgHeight}`}>
         <image href={bgSrc} x="0" y="0" width={bgWidth} height={bgHeight} />
         {children}
       </svg>
       {overlay}
     </section>
-  )
+  );
 }
 
-export default TheatreScene
+export default TheatreScene;
