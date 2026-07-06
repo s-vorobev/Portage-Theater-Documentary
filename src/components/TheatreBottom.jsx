dbd24c27443e54c256b462560f4944cd7427bd62
s-vorobev <svorobyovs357@gmail.com>
2026-07-06 00:11:02 -0500

    Fix tape placement

--- src/components/TheatreBottom.jsx
+++ src/components/TheatreBottom.jsx
@@ -1,4 +1,3 @@
-import './TheatreBottom.css'
 import TheatreScene from './scene/TheatreScene'
 import SceneAsset from './scene/SceneAsset'
 import { ASSETS } from '../lib/sceneAssets'
@@ -10,15 +9,9 @@ function TheatreBottom() {
       bgSrc="/theatre_bottom.png"
       bgWidth={6010}
       bgHeight={2580}
-      overlay={
-        <div className="content-overlay">
-          <div className="sign-zone" />
-          <div className="button-zone" />
-        </div>
-      }
     >
-      <SceneAsset asset={ASSETS.cautionTape} centerX={2605} centerY={1985} scale={2.5} rotateDeg={6} className="tape-img" />
-      <SceneAsset asset={ASSETS.cautionTape} centerX={4405} centerY={1985} scale={2.5} rotateDeg={-6} className="tape-img" />
+      <SceneAsset asset={ASSETS.cautionTape} centerX={3005} centerY={1985} scale={4.8} rotateDeg={6} />
+      <SceneAsset asset={ASSETS.cautionTape} centerX={3005} centerY={1985} scale={4.8} rotateDeg={-6} />
     </TheatreScene>
   )
 }
