0f149a0de7a9035679c09889a3c62195e952b26f
s-vorobev <svorobyovs357@gmail.com>
2026-07-13 23:13:01 -0500

    Create basic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import TheatreTop from './components/TheatreTop'
 import TheatreBottom from './components/TheatreBottom'
+import Form from './components/Form'
 
 function App() {
   return (
     <div className="scroll-container">
       <TheatreTop />
+      <Form />
       <TheatreBottom />
     </div>
   )
