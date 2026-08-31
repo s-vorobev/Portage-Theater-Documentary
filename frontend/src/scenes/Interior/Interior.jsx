c9a5a5c926fadc68ce349a03a4e510e7519bc550
s-vorobev <svorobyovs357@gmail.com>
2026-08-30 23:01:29 -0500

    Refactor frontend code

---
+++ frontend/src/scenes/Interior/Interior.jsx
@@ -0,0 +1,44 @@
+import { useNavigate } from 'react-router-dom'
+import './Interior.css'
+import TheaterScene from '../../components/TheaterScene'
+import Footage from '../../components/Footage'
+
+const FRAME_WIDTH = 4209
+const FRAME_HEIGHT = 2367
+const CUTOUT = { x: 1387, y: 565, width: 1365, height: 1155 }
+
+function Interior() {
+  const navigate = useNavigate()
+
+  return (
+    <TheaterScene
+      className="interior"
+      bgSrc="/inside_theater.png"
+      bgWidth={FRAME_WIDTH}
+      bgHeight={FRAME_HEIGHT}
+      overlay={
+        <button
+          type="button"
+          className="interior-back"
+          onClick={() => navigate('/')}
+          aria-label="Back"
+        >
+          ←
+        </button>
+      }
+    >
+      <foreignObject
+        x={CUTOUT.x}
+        y={CUTOUT.y}
+        width={CUTOUT.width}
+        height={CUTOUT.height}
+      >
+        <div xmlns="http://www.w3.org/1999/xhtml" className="interior-video-wrap">
+          <Footage className="interior-video" />
+        </div>
+      </foreignObject>
+    </TheaterScene>
+  )
+}
+
+export default Interior
